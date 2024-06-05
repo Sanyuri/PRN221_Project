b98e5b0491eda200dab5e6602982ef81cef5023a
tungpthe171795 <tungpthe171795@gmail.com>
2024-06-05 09:43:01 +0700

    Update Layout

---
+++ InterviewManagement/Pages/Shared/Footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace InterviewManagement.Pages.Shared
+{
+    public class FooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
