3317ad99072bb1634c5b6c5ff9587716084f7725
tungpthe171795 <tungpthe171795@gmail.com>
2024-06-29 09:50:37 +0700

    Add ListOffer

---
+++ InterviewManagement/Pages/ims.recruitment.com/Offers/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using InterviewManagement.Models;
+using InterviewManagement.Dtos;
+
+namespace InterviewManagement.Pages.ims.recruitment.com.Offers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly InterviewManagement.Models.InterviewManagementContext _context;
+
+        public IndexModel(InterviewManagement.Models.InterviewManagementContext context)
+        {
+            _context = context;
+        }
+
+        public IList<OfferDto> Offer { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Offer != null)
+            {
+                Offer = await _context.Offer
+                               .Include(o => o.Candidate)
+                               .Include(o => o.Department)
+                               .Include(o => o.Employees)
+                               .Select(o => new OfferDto
+                               {
+                                   Id = o.Id,
+                                   CandidateName = o.Candidate.FullName,
+                                   Email = o.Candidate.Email,
+                                   Approver = o.Employees.FirstOrDefault(e => e.Role.RoleName.Equals("Manager")).FullName,
+                                   Department = o.Department.DepertmentName,
+                                   Note = o.Note,
+                                   Status = o.Status
+                               })
+                               .ToListAsync();
+            }
+        }
+    }
+}
