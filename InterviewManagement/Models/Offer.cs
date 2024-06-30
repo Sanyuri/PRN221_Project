c9a6297870ce1969aed6966a5c176f9a28143172
tungpthe171795 <tungpthe171795@gmail.com>
2024-07-01 00:38:06 +0700

    Revert "Revert "Add Create Offer""
    This reverts commit 71ff2796ad17e6533d89def257eddecc7082bde6.

--- InterviewManagement/Models/Offer.cs
+++ InterviewManagement/Models/Offer.cs
@@ -13,23 +13,39 @@ namespace InterviewManagement.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="ContractFrom is required")]
         public DateTime? ContractFrom { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="ContractTo is required")]
         public DateTime? ContractTo { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "DueDate is required")]
         public DateTime? DueDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Salary is required")]
         public double? Salary { get; set; }
 
-        [Required]
         public bool IsDeleted { get; set; }
+
 
         [Column(TypeName = "nvarchar(max)")]
         public string? Note { get; set; }
+
+        public string? Status { get; set; }
+
+        [Required(ErrorMessage = "Candidate is required")]
+        public long? CandidateId { get; set; }
+        [Required(ErrorMessage = "Position is required")]
+        public int? PositionId { get; set; }
+        [Required(ErrorMessage = "Schedule is required")]
+        public long? ScheduleId { get; set; }
+        [Required(ErrorMessage = "Contract is required")]
+        public long? ContractId { get; set; }
+        [Required(ErrorMessage = "Department is required")]
+        public long? DepartmentId { get; set; }
+        [Required(ErrorMessage = "Level is required")]
+        public int? LevelId { get; set; }
+
 
         [ForeignKey("CandidateId")]
         public virtual Candidate? Candidate { get; set; }
@@ -42,9 +58,6 @@ namespace InterviewManagement.Models
 
         [ForeignKey("ContractId")]
         public virtual Contract? Contract { get; set; }
-
-        [ForeignKey("StatusId")]
-        public string? Status { get; set; }
 
         [ForeignKey("DepartmentId")]
         public virtual Department? Department { get; set; }
