73f3483c749c388b27caa62c8c0e3f4000d38615
ミ。Herniel <142606461+Sanyuri@users.noreply.github.com>
2024-07-14 15:47:19 +0700

    ChangePassword for logged in user

--- InterviewManagement/Pages/ims.recruitment.com/ResetPassword.cshtml.cs
+++ InterviewManagement/Pages/ims.recruitment.com/ResetPassword.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Claims;
 
 namespace InterviewManagement.Pages.ims.recruitment.com
 {
@@ -33,33 +34,51 @@ namespace InterviewManagement.Pages.ims.recruitment.com
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
+            var AccountId = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Name)?.Value;
+            if (AccountId == null)
             {
-                return BadRequest(ModelState);
-            }
+                //Reset password for forgetpassword function
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
 
-            Employee? employee = await _context.Employee.Where(e => e.ResetPasswordToken.Equals(ResetPasword.Token)).FirstOrDefaultAsync();
-            if(employee == null)
-            {
-                ModelState.AddModelError(string.Empty, "Cannot update user's password");
-                return Page();
+                Employee? employee = await _context.Employee.Where(e => e.ResetPasswordToken.Equals(ResetPasword.Token)).FirstOrDefaultAsync();
+                if (employee == null)
+                {
+                    ModelState.AddModelError(string.Empty, "Cannot update user's password");
+                    return Page();
+                }
+                if (!ResetPasword.Password.Equals(ResetPasword.RePassword))
+                {
+                    ModelState.AddModelError(string.Empty, "Password and Confirm password don’t match. Please try again.");
+                    return Page();
+                }
+                if (employee.ExpiredDate < DateTime.Now)
+                {
+                    ModelState.AddModelError(string.Empty, "This link has expired. Please go back to Homepage and try again.");
+                    return Page();
+                }              
+                employee.ResetPasswordToken = null;
+                employee.ExpiredDate = null;
+                employee.Password = BCrypt.Net.BCrypt.HashPassword(ResetPasword.Password);
+                _context.Attach(employee).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return RedirectToPage("/Index");
             }
-            if (!ResetPasword.Password.Equals(ResetPasword.RePassword))
+            else
             {
-                ModelState.AddModelError(string.Empty, "Password and Confirm password don’t match. Please try again.");
-                return Page();
-            }
-            if(employee.ExpiredDate < DateTime.Now)
-            {
-                ModelState.AddModelError(string.Empty, "This link has expired. Please go back to Homepage and try again.");
-                return Page();
-            }
-            employee.Password = BCrypt.Net.BCrypt.HashPassword(ResetPasword.Password);
-            employee.ResetPasswordToken = null;
-            employee.ExpiredDate = null;
-            _context.Attach(employee).State = EntityState.Modified;
-            await _context.SaveChangesAsync();
-            return RedirectToPage("/Index");
+                //Reset password for user's reset password function
+                Employee? employee = await _context.Employee.Where(e => e.Id == long.Parse(AccountId)).FirstOrDefaultAsync();
+                if(employee == null)
+                {
+                    return NotFound();
+                }
+                employee.Password = BCrypt.Net.BCrypt.HashPassword(ResetPasword.Password);
+                _context.Attach(employee).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+                return RedirectToPage("/ims.recruitment.com/HomePage");
+            }           
         }
     }
 }
